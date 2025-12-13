1c51f289ed2b27de60913201de46ff4324621d4f
Hieronymus <hieronymus@git.com>
2025-12-14 00:39:58 +0800

    add a new note

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -39,51 +39,63 @@ export default defineConfig({
         ]
       },
       {
-        text: 'Milionaire Mind Intensive (MMI)',
+        text: 'Investment Notes',
         items: [
-          { text: 'Passive income', link: '/MMI-1' },
-          { text: 'Financial Freedom Formular', link: '/MMI-4' },
-          { text: '6 jars', link: '/MMI-2' },
-          { text: 'Method for companies', link: '/MMI-3' },
-          { text: 'Method for families', link: '/MMI-5' },
-          { text: 'Necessary actions to be rich', link: '/MMI-6' },
-          { text: 'Money Personality Types', link: '/MMI-7' },
+          { text: 'Interest Rate', link: '/interest-rate' },
+          {
+            text: 'Milionaire Mind Intensive (MMI)',
+            items: [
+              { text: 'Passive income', link: '/MMI-1' },
+              { text: 'Financial Freedom Formular', link: '/MMI-4' },
+              { text: '6 jars', link: '/MMI-2' },
+              { text: 'Method for companies', link: '/MMI-3' },
+              { text: 'Method for families', link: '/MMI-5' },
+              { text: 'Necessary actions to be rich', link: '/MMI-6' },
+              { text: 'Money Personality Types', link: '/MMI-7' },
+            ]
+          },
         ]
       },
       {
-        text: 'O-Level Math Notes',
+        text: 'O-Level Notes',
         items: [
-          { text: 'Quadratic', link: '/A-M-1' },
-          { text: 'Logarithm', link: '/A-M-2' },
-          { text: 'Polynomial', link: '/A-M-3' },
-          { text: 'Partial fraction', link: '/A-M-4' },
-          { text: 'Geometry', link: '/A-M-5' },
-          { text: 'Binomial', link: '/A-M-6' },
-          { text: 'Trigonometry', link: '/A-M-7' },
-          { text: 'Calculus', link: '/A-M-8' },
+
+          {
+            text: 'Mathematic',
+            items: [
+              { text: 'Quadratic', link: '/A-M-1' },
+              { text: 'Logarithm', link: '/A-M-2' },
+              { text: 'Polynomial', link: '/A-M-3' },
+              { text: 'Partial fraction', link: '/A-M-4' },
+              { text: 'Geometry', link: '/A-M-5' },
+              { text: 'Binomial', link: '/A-M-6' },
+              { text: 'Trigonometry', link: '/A-M-7' },
+              { text: 'Calculus', link: '/A-M-8' },
+            ]
+          },
+          {
+            text: 'Physics',
+            items: [
+              { text: 'Base', link: '/Physics-Lesson-4' },
+              { text: 'Force', link: '/Physics-Lesson-1' },
+              { text: 'Heat', link: '/Physics-Lesson-3' },
+              { text: 'Energy', link: '/Physics-Lesson-5' },
+              { text: 'Wave', link: '/Physics-Lesson-6' },
+              { text: 'Light', link: '/Physics-Lesson-7' },
+              { text: 'Electricity', link: '/Physics-Lesson-8' },
+              { text: 'Magnetism', link: '/Physics-Lesson-12' },
+            ]
+          },
+          {
+            text: 'English',
+            items: [
+              { text: 'Grammar', link: '/English-Lesson-1' },
+              { text: 'Letter format', link: '/letter-format' },
+              { text: 'HLAI', link: '/HLAI' }
+            ]
+          }
         ]
       },
-      {
-        text: 'O-Level Physics Notes',
-        items: [
-          { text: 'Base', link: '/Physics-Lesson-4' },
-          { text: 'Force', link: '/Physics-Lesson-1' },
-          { text: 'Heat', link: '/Physics-Lesson-3' },
-          { text: 'Energy', link: '/Physics-Lesson-5' },
-          { text: 'Wave', link: '/Physics-Lesson-6' },
-          { text: 'Light', link: '/Physics-Lesson-7' },
-          { text: 'Electricity', link: '/Physics-Lesson-8' },
-          { text: 'Magnetism', link: '/Physics-Lesson-12' },
-        ]
-      },
-      {
-        text: 'O-Level English Notes',
-        items: [
-          { text: 'Grammar', link: '/English-Lesson-1' },
-          { text: 'Letter format', link: '/letter-format' },
-          { text: 'HLAI', link: '/HLAI' }
-        ]
-      }
     ],
   },
 })
---
+++ docs/interest-rate.md
@@ -0,0 +1,52 @@
+# Interest Rate
+## The difference between investment and financial management
+
+1. Investment is high reward with high risk.
+2. Financial management is low reward with low risk.
+
+### Bill Gates's Portfolio
+
+Pay 10 billion for Interest-free government bonds at a 75% discount.
+
+||||
+|-|-|-|
+|7.5 billion|Will return after 10 years.|***financial management***|
+|2.5 billion|Investment with high risk|***investment***|
+
+### 3 rules for investment
+1. Keep the pricipal!
+2. Keep the pricipal!
+3. Keep the pricipal!
+
+## 4 things for taking charge of your property
+1. Interest rate
+2. Tax rate
+3. Multiply assets
+4. Turning liabilities to assets
+<br><br>
+
+## Interest
+
+**Is your speed of earning faster than speed of interest rate changing?**
+
+*Keep your pricipal or thin your pricipal?*
+
+***Interest rate is a key number which effecting your fortune.***
+
+- High interest rate means inflation.
+- Low interest rate means they do not want anybody to deposit in banks.
+- Low interest rate also means developed.
+
+## Aging in China
+
+|Demographic dividend|Population Liability|
+|-|-|
+|Baby boom|The elderly boom|
+|Many the youngs|Many the olds|
+|Good economy|Bad economy|
+|Inflation|Deflation|
+
+The most conventional way for financial management is care the old by their children.
+But their children will work for the goverment, the bank and the bussiness owner.
+
+China will enter into the state of being old before being rich and severe declining birth rate.
